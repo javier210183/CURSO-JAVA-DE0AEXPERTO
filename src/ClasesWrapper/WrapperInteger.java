4fdcad479025d09abc055d2d5dd38c2fee1b6af9
Javier Sanchez Collado <colladonhijodecolladan@gmail.com>
2025-08-01 12:02:07 +0200

    wrapper

---
+++ src/ClasesWrapper/WrapperInteger.java
@@ -0,0 +1,33 @@
+package ClasesWrapper;
+
+public class WrapperInteger {
+    public static void main(String[] args) {
+
+        //transformamos integer en primitivo
+        int intPrimitivo = 32768;
+        Integer intObjeto = Integer.valueOf(intPrimitivo);
+        Integer intObjeto2 = intPrimitivo;
+        System.out.println("intObjeto = " + intObjeto);
+
+        //transformamos un primitivo a integer
+        int num = intObjeto;
+        int num2 = intObjeto.intValue();
+        System.out.println("num2 = " + num2);
+
+        //De string a valor entero
+        String valorTvLcd = "67000";
+        Integer valor = Integer.valueOf(valorTvLcd);
+        System.out.println("valor = " + valor);
+
+        //PERDIDAS DE INFORMACION AL SUPERAR LOS MAXIMOS DE LOS TIPOS 
+        Short shortObjeto = intObjeto.shortValue();
+        System.out.println("shortObjeto = " + shortObjeto);
+
+        Byte byteObjeto = intObjeto.byteValue();
+        System.out.println("byteObjeto = " + byteObjeto);
+        
+        Long longObjeto = intObjeto.longValue();
+        System.out.println("longObjeto = " + longObjeto);
+
+    }
+}
