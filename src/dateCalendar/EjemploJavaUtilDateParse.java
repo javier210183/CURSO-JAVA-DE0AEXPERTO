8c5639a518ecdfa257359dab72d77048c067f2fa
Javier Sanchez Collado <colladonhijodecolladan@gmail.com>
2025-08-06 20:37:59 +0200

    DateCalendar(fechas y calendario)+(comparacion de fechas)

--- src/dateCalendar/EjemploJavaUtilDateParse.java
+++ src/dateCalendar/EjemploJavaUtilDateParse.java
@@ -12,11 +12,22 @@ public class EjemploJavaUtilDateParse {
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
         //Convertimos un String del usuario a un objeto fecha
-        System.out.println("Ingrese una fecha con formato : 'yyyy-MM-dd'");
+        System.out.println("Ingrese una fecha con formato : 'yyyy-MM-dd'\n");
         try {
             Date fecha = format.parse(s.next());
             System.out.println("fecha = " + fecha);
             System.out.println("format = " + format.format(fecha));
+
+            Date fecha2 = new Date();
+            System.out.println("fecha2 = " + fecha2);
+
+            if(fecha.after(fecha2)){
+                System.out.println("Fecha1 (del usuario) es posterior que fecha2 (actual)");
+            }else if(fecha.before(fecha2)){
+                System.out.println("Fecha es anterior que fecha2");
+            }else if(fecha.equals(fecha2)){
+                System.out.println("fecha es igual a fecha2");
+            }
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
