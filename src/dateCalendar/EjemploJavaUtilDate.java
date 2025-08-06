74bf34bc04c1f80d4e5bf55e853edf2cec65284f
Javier Sanchez Collado <colladonhijodecolladan@gmail.com>
2025-08-06 19:03:47 +0200

    DateCalendar

---
+++ src/dateCalendar/EjemploJavaUtilDate.java
@@ -0,0 +1,12 @@
+package dateCalendar;
+
+import java.util.Date;
+
+public class EjemploJavaUtilDate {
+    public static void main(String[] args) {
+
+        Date fecha =  new Date();
+
+        System.out.println("fecha = " + fecha);
+    }
+}
