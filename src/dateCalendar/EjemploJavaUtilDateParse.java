2f6f53455534c0a073d6942844baf2dce25f97aa
Javier Sanchez Collado <colladonhijodecolladan@gmail.com>
2025-09-09 21:19:53 +0200

    DateCalendar(fechas y calendario)+(comparacion de fechas)

--- src/dateCalendar/EjemploJavaUtilDateParse.java
+++ src/dateCalendar/EjemploJavaUtilDateParse.java
@@ -18,6 +18,7 @@ public class EjemploJavaUtilDateParse {
         Date fecha = null;
 
         try {
+
             fecha = format.parse(s.next());
             System.out.println("fecha = " + fecha);
             System.out.println("format = " + format.format(fecha));
